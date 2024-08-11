f5638ecde0eae04e412a2fa42d8285f9c91cdf5d
Marshall Ku <marshall@kakao.com>
2024-08-11 21:37:21 +0900

    Modify to response svg instead json

--- src/controllers/index.rs
+++ src/controllers/index.rs
@@ -1,4 +1,5 @@
-use axum::{extract::Query, response::IntoResponse};
+use axum::{extract::Query, http::HeaderMap, response::IntoResponse};
+use reqwest::StatusCode;
 use serde::Deserialize;
 
 use crate::services::index::index_service;
@@ -10,5 +11,14 @@ pub struct Options {
 }
 
 pub async fn get(Query(Options { user_name, year }): Query<Options>) -> impl IntoResponse {
-    index_service(user_name, year).await
+    let mut headers = HeaderMap::new();
+
+    headers.insert("Content-Type", "image/svg+xml".parse().unwrap());
+    headers.insert("Cache-Control", "no-cache".parse().unwrap());
+    headers.insert("Pragma", "no-cache".parse().unwrap());
+    headers.insert("Expires", "0".parse().unwrap());
+
+    let response = index_service(user_name, year).await;
+
+    (StatusCode::OK, headers, response)
 }
--- src/services/index.rs
+++ src/services/index.rs
@@ -1,8 +1,18 @@
-use axum::{response::IntoResponse, Json};
-
 use crate::api::contributions::get_daily_commits;
 
-pub async fn index_service(user_name: String, year: i32) -> impl IntoResponse {
+pub async fn index_service(user_name: String, year: i32) -> String {
     let response = get_daily_commits(&user_name, year).await.unwrap();
-    Json(response)
+    let svg = r##"
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 930 465"
+        fill="none"
+    >
+    </svg>
+    "##;
+
+    println!("{:?}", response);
+
+    svg.to_string()
 }
