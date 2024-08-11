5a7d924eb80200bf713672fe5ab4edd2ecfbcf09
Marshall Ku <marshall@kakao.com>
2024-08-11 18:36:19 +0900

    Verify index page

--- src/controllers/__tests__/index.rs
+++ src/controllers/__tests__/index.rs
@@ -12,10 +12,26 @@ mod tests {
     use crate::controllers::index;
 
     #[tokio::test]
-    async fn should_response() {
+    async fn should_not_response_without_queries() {
         let app: Router = Router::new().route("/", get(index::get));
         let response = app
             .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[tokio::test]
+    async fn should_response_with_queries() {
+        let app: Router = Router::new().route("/", get(index::get));
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/?user_name=marshallku&year=2024")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
             .await
             .unwrap();
 
