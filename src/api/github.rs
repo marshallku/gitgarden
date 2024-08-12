83684e4bf37fd833ca7427618719be841a5e1520
Marshall Ku <marshall@kakao.com>
2024-08-12 17:04:53 +0900

    Add logics for making graphql request to GitHub

---
+++ src/api/github.rs
@@ -0,0 +1,48 @@
+use reqwest::{Client, Error};
+use serde_json::{json, Value};
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+pub async fn github_graphql_request(
+    query: &str,
+    headers: &HashMap<&str, &str>,
+    data: Value,
+    token: &str,
+) -> Result<Value, Error> {
+    let client = Client::new();
+
+    let mut request_headers = reqwest::header::HeaderMap::new();
+    request_headers.insert(
+        reqwest::header::AUTHORIZATION,
+        format!("token {}", token).parse().unwrap(),
+    );
+
+    for (key, value) in headers {
+        request_headers.insert(
+            reqwest::header::HeaderName::from_bytes(key.as_bytes()).unwrap(),
+            value.parse().unwrap(),
+        );
+    }
+
+    let mut body = json!({
+        "query": query
+    });
+
+    // Merge the additional data into the body
+    if let Some(obj) = body.as_object_mut() {
+        if let Some(data_obj) = data.as_object() {
+            obj.extend(data_obj.clone());
+        }
+    }
+
+    let response = client
+        .post("https://api.github.com/graphql")
+        .headers(request_headers)
+        .json(&body)
+        .send()
+        .await?;
+
+    let json_response: Value = response.json().await?;
+
+    Ok(json_response)
+}
