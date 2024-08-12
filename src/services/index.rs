d81a24a942de60555abf1fbf7a09bd1cfc177f40
Marshall Ku <marshall@kakao.com>
2024-08-12 10:35:53 +0900

    Modify color of cells with commit level

--- src/services/index.rs
+++ src/services/index.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use crate::api::contributions::get_daily_commits;
-use chrono::{Datelike, Duration, NaiveDate, Weekday};
+use chrono::{Datelike, Duration, NaiveDate};
 
 const CELL_SIZE: u32 = 10;
 const CELL_SPACING: u32 = 4;
@@ -12,7 +14,7 @@ pub async fn index_service(user_name: String, year: i32) -> String {
     let (start_date, end_date) = get_year_range(year);
     let weeks = calculate_weeks(start_date, end_date);
 
-    let cells = generate_contribution_cells(start_date, weeks, year);
+    let cells = generate_contribution_cells(year, start_date, weeks, commits);
 
     generate_svg(cells)
 }
@@ -30,19 +32,29 @@ fn calculate_weeks(start_date: NaiveDate, end_date: NaiveDate) -> usize {
     ((end_date - start_date).num_days() / WEEK_TO_DAY as i64 + 1) as usize
 }
 
-fn generate_contribution_cells(start_date: NaiveDate, weeks: usize, year: i32) -> String {
+fn generate_contribution_cells(
+    year: i32,
+    start_date: NaiveDate,
+    weeks: usize,
+    commits: HashMap<String, u32>,
+) -> String {
     let mut cells = String::new();
 
     for week in 0..weeks {
         for day in 0..WEEK_TO_DAY {
             let current_date = start_date + Duration::days((week * WEEK_TO_DAY + day) as i64);
+            let formatted_date = current_date.format("%Y-%m-%d").to_string();
+
             let x = week as u32 * (CELL_SIZE + CELL_SPACING) + GRID_LEFT_PADDING;
             let y = day as u32 * (CELL_SIZE + CELL_SPACING) + GRID_TOP_PADDING;
-            let color = get_cell_color(current_date, year);
+
+            let commit_level = commits.get(&formatted_date).unwrap_or(&0);
+            let is_same_year = current_date.year() == year;
+            let color = get_cell_color(is_same_year, commit_level);
 
             cells.push_str(&format!(
-                "  <rect width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" fill=\"{}\" title=\"{}\" />\n",
-                CELL_SIZE, CELL_SIZE, x, y, color, current_date.format("%Y-%m-%d")
+                "  <rect width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" fill=\"{}\" title=\"{}\" data-level=\"{}\" />\n",
+                CELL_SIZE, CELL_SIZE, x, y, color, formatted_date, commit_level
             ));
         }
     }
@@ -50,14 +62,17 @@ fn generate_contribution_cells(start_date: NaiveDate, weeks: usize, year: i32) -
     cells
 }
 
-fn get_cell_color(date: NaiveDate, year: i32) -> &'static str {
-    if date.year() == year {
-        match date.weekday() {
-            Weekday::Sat | Weekday::Sun => "#ebedf0",
-            _ => "#9be9a8",
-        }
+fn get_cell_color(is_same_year: bool, commit_level: &u32) -> &'static str {
+    if !is_same_year {
+        "#ffffff"
     } else {
-        "#ffffff" // Color for days outside the year
+        match commit_level {
+            1 => "#c6e48b",
+            2 => "#7bc96f",
+            3 => "#239a3b",
+            4 => "#196127",
+            _ => "#ebedf0",
+        }
     }
 }
 
