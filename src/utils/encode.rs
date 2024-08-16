44fdf9982412d9bebc46348eaf01e17137f3ead4
Marshall Ku <marshall@kakao.com>
2024-08-16 22:10:36 +0900

    Add morr logs for debugging

--- src/utils/encode.rs
+++ src/utils/encode.rs
@@ -5,10 +5,10 @@ use base64::{engine::general_purpose, Engine as _};
 pub fn encode_from_path(path: &str) -> String {
     let assets_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("assets");
     let file_path = assets_path.join(path);
-    let file = std::fs::read(file_path);
+    let file = std::fs::read(file_path.clone());
 
     if let Err(e) = file {
-        eprintln!("Error reading file: {}, {:?}", path, e);
+        eprintln!("Error reading file: {}, {:?}", file_path.to_str().unwrap_or(path), e);
         String::new()
     } else {
         let encoded = general_purpose::STANDARD_NO_PAD.encode(file.unwrap());
