c259ac129a1ca372ceae4f74a411d98726ba05be
Marshall Ku <marshall@kakao.com>
2024-08-20 07:44:51 +0900

    Add coordinate in Home

--- src/render/home.rs
+++ src/render/home.rs
@@ -1,34 +1,42 @@
-use crate::utils::{coordinate::generate_coordinate, encode::encode_from_path};
+use crate::utils::{
+    coordinate::{generate_coordinate, Rectangle},
+    encode::encode_from_path,
+};
 
 use super::renderable::Renderable;
 
 pub struct Home {
-    user_name: String,
+    pub coordinate: Rectangle,
 }
 
 impl Home {
     pub fn new(user_name: &str) -> Self {
+        let (x, y) = generate_coordinate(&user_name, (80.0, 730.0), (25.0, 70.0), None).unwrap();
+
         Self {
-            user_name: user_name.to_string(),
+            coordinate: Rectangle {
+                x1: x,
+                y1: y,
+                x2: x + 67.0,
+                y2: y + 152.0,
+            },
         }
     }
 }
 
 impl Renderable for Home {
     fn render(&self) -> String {
-        let (x, y) =
-            generate_coordinate(&self.user_name, (80.0, 730.0), (25.0, 70.0), None).unwrap();
         let home = encode_from_path("objects/home.png");
         let road = encode_from_path("objects/stone_road.png");
 
         format!(
             r#"<image width="151" height="155" x="{}" y="{}" xlink:href="data:image/png;base64,{}" />
             <image width="31" height="89" x="{}" y="{}" xlink:href="data:image/png;base64,{}" />"#,
-            x,
-            y,
+            self.coordinate.x1,
+            self.coordinate.y1,
             home,
-            x + 67.0,
-            y + 152.0,
+            self.coordinate.x2,
+            self.coordinate.y2,
             road
         )
     }
