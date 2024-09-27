dc6779441031e772c35d24edb6aac71924581385
Marshall Ku <marshall@kakao.com>
2024-09-27 20:34:31 +0900

    Add error for Rgb

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -7,19 +7,27 @@ pub struct Rgb {
     pub blue: u8,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum RgbError {
+    #[error("Invalid hex color format. Use #RRGGBB.")]
+    InvalidFormat,
+    #[error("Invalid hex value.")]
+    InvalidHexValue,
+}
+
 impl Rgb {
     pub fn new(red: u8, green: u8, blue: u8) -> Self {
         Rgb { red, green, blue }
     }
 
-    pub fn from_hex(hex: &str) -> Result<Self, String> {
+    pub fn from_hex(hex: &str) -> Result<Self, RgbError> {
         if hex.len() != 7 || !hex.starts_with('#') {
-            return Err("Invalid hex color format. Use #RRGGBB.".to_string());
+            return Err(RgbError::InvalidFormat);
         }
 
-        let r = u8::from_str_radix(&hex[1..3], 16).map_err(|_| "Invalid hex value for red.")?;
-        let g = u8::from_str_radix(&hex[3..5], 16).map_err(|_| "Invalid hex value for green.")?;
-        let b = u8::from_str_radix(&hex[5..7], 16).map_err(|_| "Invalid hex value for blue.")?;
+        let r = u8::from_str_radix(&hex[1..3], 16).map_err(|_| RgbError::InvalidHexValue)?;
+        let g = u8::from_str_radix(&hex[3..5], 16).map_err(|_| RgbError::InvalidHexValue)?;
+        let b = u8::from_str_radix(&hex[5..7], 16).map_err(|_| RgbError::InvalidHexValue)?;
 
         Ok(Rgb::new(r, g, b))
     }
