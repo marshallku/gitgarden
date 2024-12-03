b12ee2c4d720e617ccae19cbf75c9998c295da46
Marshall Ku <marshall@kakao.com>
2024-12-03 12:33:21 +0900

    Refactor mask registration logics

--- src/render/objects.rs
+++ src/render/objects.rs
@@ -78,11 +78,15 @@ impl Objects {
         .into_iter()
     }
 
-    pub fn get_mask_id(&self) -> Option<String> {
+    pub fn get_mask_path(&self) -> Option<&'static str> {
         match self {
-            Objects::FlowerThree => Some(format!("mask-{}", Objects::FlowerThree.to_string())),
-            Objects::FlowerFour => Some(format!("mask-{}", Objects::FlowerFour.to_string())),
+            Objects::FlowerThree => Some("M0.4375 0.25h0.0625V0.1875h0.0625V0.125h0.25v0.125h0.125v0.1875h-0.0625v0.0625h-0.0625v0.0625h-0.1875V0.5H0.5625V0.4375H0.5V0.375H0.4375V0.25z"),
+            Objects::FlowerFour => Some("M0.0625 0.3125h0.06375v-0.0625H0.5v-0.061875L0.5625 0.1875v-0.0625h0.25v0.061875L0.875 0.1875v0.0625h0.0625v0.0625h0.0625v0.438125L0.9375 0.75v0.0625h-0.3125v0.0625h-0.0625v0.0625h-0.121875L0.4375 0.875h-0.0625v-0.0625h-0.25v-0.0625h-0.0625v-0.4375z"),
             _ => None,
         }
+    }
+
+    pub fn get_mask_id(&self) -> Option<String> {
+        self.get_mask_path().map(|_| format!("mask-{}", self.to_string()))
     }
 }
