b5f121460767a384795b0cb7bc1dc338eee0bbd7
Marshall Ku <marshall@kakao.com>
2024-12-11 08:21:16 +0900

    Improve minifying logics with removing more unnecessary spaces

--- src/utils/__tests__/string.rs
+++ src/utils/__tests__/string.rs
@@ -23,7 +23,40 @@ mod tests {
 </svg>
 "#;
 
-        let minified_xml = r#"<svg> <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
+        let minified_xml = r#"<svg><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
+
+        assert_eq!(minify_xml(xml), minified_xml);
+    }
+
+    #[test]
+    fn test_removing_spaces_between_attributes() {
+        let xml = r#"
+           <svg>
+                <circle                       cx="50"               cy="50"  r="40"   stroke="black" stroke-width="3" fill="red" />
+            </svg>
+        "#;
+
+        let minified_xml = r#"<svg><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
+
+        assert_eq!(minify_xml(xml), minified_xml);
+    }
+
+    #[test]
+    fn test_removing_unnecessary_spaces() {
+        let xml = r#"
+           <svg>
+                <def>
+                    <style>
+                        .red {
+                            fill: red;
+                        }
+                    </style>
+                </def>
+                <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+            </svg>
+        "#;
+
+        let minified_xml = r#"<svg><def><style>.red{fill:red;}</style></def><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
 
         assert_eq!(minify_xml(xml), minified_xml);
     }
