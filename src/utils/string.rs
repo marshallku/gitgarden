846f8e634a4e589a4564156f49d16a9a354a42e1
Marshall Ku <marshall@kakao.com>
2024-12-13 23:33:35 +0900

    Revert "Improve minifying logics with removing more unnecessary spaces"
    This reverts commit b5f121460767a384795b0cb7bc1dc338eee0bbd7.

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -1,37 +1,18 @@
 pub fn minify_xml(s: &str) -> String {
-    let simply_minified = s.replace("\n", "");
-    let simply_minified = simply_minified.trim();
-    let mut minified = String::with_capacity(simply_minified.len());
+    let without_linebreak = s.replace("\n", "");
+    let mut minified = String::with_capacity(without_linebreak.len());
     let mut space_count = 0;
-    let mut prev_char = '\0';
 
-    for (i, c) in simply_minified.chars().enumerate() {
-        let next_char = simply_minified.chars().nth(i + 1).unwrap_or('\0');
-
+    for c in without_linebreak.chars() {
         if c == ' ' {
             space_count += 1;
-            if space_count == 1
-                && prev_char != '>'
-                && next_char != '<'
-                && prev_char != '='
-                && next_char != '='
-                && prev_char != ':'
-                && next_char != ':'
-                && prev_char != ';'
-                && next_char != ';'
-                && prev_char != '{'
-                && next_char != '}'
-                && prev_char != '}'
-                && next_char != '{'
-            {
+            if space_count == 1 {
                 minified.push(c);
             }
         } else {
             space_count = 0;
             minified.push(c);
         }
-
-        prev_char = c;
     }
 
     minified
