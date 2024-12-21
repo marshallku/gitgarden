22c5f41b75868e4c50cc59edac8d3c15af09dfac
Marshall Ku <marshall@kakao.com>
2024-12-21 15:18:07 +0900

    Verify compression of styles

--- src/utils/__tests__/string.rs
+++ src/utils/__tests__/string.rs
@@ -23,7 +23,27 @@ mod tests {
 </svg>
 "#;
 
-        let minified_xml = r#"<svg> <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
+        let minified_xml = r#"<svg><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
+
+        assert_eq!(minify_xml(xml), minified_xml);
+    }
+
+    #[test]
+    fn test_removing_spaces_in_styles() {
+        let xml = r#"
+<svg>
+    <defs>
+        <style>
+            .cls-1 {
+                fill: red;
+            }
+        </style>
+    </defs>
+    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+</svg>
+"#;
+
+        let minified_xml = r#"<svg><defs><style>.cls-1{fill:red;}</style></defs><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>"#;
 
         assert_eq!(minify_xml(xml), minified_xml);
     }
