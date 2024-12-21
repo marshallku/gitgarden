593df2701c9abd7deaa208067162ed5e77c7d4f7
Marshall Ku <marshall@kakao.com>
2024-12-21 15:17:58 +0900

    Improve compression rate

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -2,9 +2,26 @@ pub fn minify_xml(s: &str) -> String {
     let without_linebreak = s.replace("\n", "");
     let mut minified = String::with_capacity(without_linebreak.len());
     let mut space_count = 0;
+    let mut prev_char = '\0';
+    let chars = without_linebreak.chars().collect::<Vec<_>>();
 
-    for c in without_linebreak.chars() {
+    for (i, &c) in chars.iter().enumerate() {
+        let next_char = chars.get(i + 1).unwrap_or(&'\0');
         if c == ' ' {
+            if prev_char == '>'
+                || prev_char == '{'
+                || prev_char == '}'
+                || prev_char == ';'
+                || prev_char == ':'
+                || *next_char == '<'
+                || *next_char == '{'
+                || *next_char == '}'
+                || *next_char == ';'
+                || *next_char == ':'
+            {
+                continue;
+            }
+
             space_count += 1;
             if space_count == 1 {
                 minified.push(c);
@@ -12,6 +29,7 @@ pub fn minify_xml(s: &str) -> String {
         } else {
             space_count = 0;
             minified.push(c);
+            prev_char = c;
         }
     }
 
